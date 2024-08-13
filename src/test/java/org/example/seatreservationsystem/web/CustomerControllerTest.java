86edf8ea1edcbff5a8bf5bf23e19bfd5c5e1cda5
Ekene Innocent Onyemanjo <innoscisco@yahoo.com>
2024-08-12 18:29:38 -0700

    Testcases Added

---
+++ src/test/java/org/example/seatreservationsystem/web/CustomerControllerTest.java
@@ -0,0 +1,24 @@
+package org.example.seatreservationsystem.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+
+    @Test
+    void viewHomePage() {
+    }
+
+    @Test
+    void saveCustomer() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+}
